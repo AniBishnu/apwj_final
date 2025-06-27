aa385a739ce7cdbf0d2ccb8c2e69d69d443fef2b
AniBishnu <120014995+AniBishnu@users.noreply.github.com>
2025-06-27 23:53:26 +0600

    Project 4th

---
+++ Project/Vitascript/src/main/java/com/example/Vitascript/Entity/DoctorType.java
@@ -0,0 +1,27 @@
+package com.example.Vitascript.Entity;
+
+public class DoctorType {
+    public int id;
+    public String name;
+
+    public DoctorType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
